70c02b9b59aab98f076bbcda80e4b7b19e61051c
RobertAlfonzo <susejrobert@gmail.com>
2024-10-03 22:43:05 -0400

    added react-navigator

--- components/Main.jsx
+++ components/Main.jsx
@@ -5,152 +5,38 @@ import {
   ActivityIndicator,
   StyleSheet,
   ImageBackground,
-  TouchableHighlight
+  ScrollView,
+  VirtualizedList
 } from 'react-native';
 import Musics from './Musics.jsx';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { getPermission, getSound } from '../lib/files.js';
-import * as MediaLibrary from 'expo-media-library';
-import { Link, Stack } from 'expo-router';
-import { Plane } from './Plane.jsx';
-import { createAudioApp, pauseAudio } from '../lib/playAudio.js';
 import Reproductor from './Reproductor.jsx';
-import { duration } from '../lib/duration.js';
-import { SearchIcon } from './Icons.jsx';
 import Search from './SearchAudio.jsx';
-export default function Main() {
-  const [permissionResponse, requestPermission] = MediaLibrary.usePermissions();
-  const [albums, setAlbums] = useState();
-  const [pages, setPages] = useState(500);
-  const [morePages, setMorePages] = useState(true);
-  const [loading, setLoading] = useState(true); //pasar a false
-  const [reproductor, setReproductor] = useState(false);
-  const [fileId, setFileId] = useState(null);
-  const [fileAudio, setFileAudio] = useState(null);
-  const [status, setStatus] = useState(true);
-  const [fileName, setFileName] = useState(null);
-  const [albumSound, setAlbumSound] = useState([]); //<---ojo
-  const [randomMode, setRandomMode] = useState(false);
-  const [isSearch, setIsSearch] = useState(true);
-  const [listAudio, setListAudio] = useState([]);
-  const [positionAudio, setPositionAudio] = useState(0);
-  const [minutes, setMinutes] = useState(null);
+export default function Main({
+  fileId,
+  albums,
+  handleAlbums,
+  createAudio,
+  handleFile,
+  fileAudio,
+  isPlaying,
+  status,
+  changeSound,
+  randomList,
+  albumSound,
+  randomMode,
+  backSound,
+  handlePosition,
+  positionAudio,
+  rangeProcess,
+  reproductor,
+  isSearch,
+  fileName
+}) {
   const insets = useSafeAreaInsets();
-  useEffect(() => {
-    getPermission(permissionResponse, requestPermission).then((assets) => {
-      setAlbums(assets);
-      setListAudio(assets);
-      setAlbumSound(
-        assets.map((obj) => {
-          return parseInt(obj.id);
-        })
-      );
-    });
-  }, [permissionResponse, requestPermission]);
 
-  const handleFile = (id, render = true) => {
-    const num = id;
-    setFileId(num);
-    setReproductor(render);
-  };
-
-  const handlePosition = (range, num) => {
-    setMinutes(num);
-    setPositionAudio(range);
-  };
-
-  const createAudio = (uri, name, num) => {
-    setMinutes(num && num);
-    setFileName(name);
-    if (fileAudio) {
-      pauseAudio(fileAudio);
-    }
-    createAudioApp(uri).then((file) => {
-      setFileAudio(file);
-      rangeProcess(file, num);
-    });
-    setStatus(false);
-  };
-
-  const rangeProcess = (file, num) => {
-    if (minutes) {
-      setInterval(async () => {
-        const currentStatus = await file.getStatusAsync();
-        if (currentStatus.isPlaying) {
-          setPositionAudio(currentStatus.positionMillis / 1000);
-        }
-      }, 500);
-    } else if (num) {
-      setInterval(async () => {
-        const currentStatus = await file.getStatusAsync();
-        if (currentStatus.isPlaying) {
-          setPositionAudio(currentStatus.positionMillis / 1000);
-        }
-      }, 500);
-    }
-  };
-  const isPlaying = (state) => {
-    setStatus(state);
-  };
-
-  const backSound = async (id) => {
-    clearInterval();
-    setPositionAudio(0);
-    const num = albumSound[albumSound.indexOf(id) - 1];
-    setFileId(num);
-    const objectSound = await getSound(num).then((assets) => {
-      setMinutes(assets[0].duration);
-      createAudio(assets[0].uri, assets[0].filename);
-      return {
-        sound: assets[0],
-        seconds: duration(assets[0].duration),
-        num
-      };
-    });
-    return objectSound;
-  };
-
-  const changeSound = async (id) => {
-    clearInterval();
-    const num = albumSound[albumSound.indexOf(id) + 1];
-    setFileId(num);
-    const objectSound = await getSound(num).then((assets) => {
-      createAudio(assets[0].uri, assets[0].filename);
-      setMinutes(assets[0].duration);
-      setPositionAudio(0);
-      return {
-        sound: assets[0],
-        seconds: duration(assets[0].duration),
-        num
-      };
-    });
-    return objectSound;
-  };
-  if (positionAudio >= parseInt(minutes) - 0.5) {
-    setPositionAudio(0);
-    changeSound(fileId);
-  }
-  const randomList = (isRandom) => {
-    if (isRandom) {
-      setAlbumSound(albumSound.sort(() => Math.random() - 0.5));
-      setRandomMode(true);
-    } else {
-      setAlbumSound(albumSound.sort((a, b) => a - b));
-      setRandomMode(false);
-    }
-  };
-
-  const handleAlbums = (nameAudio) => {
-    const name = nameAudio.toLowerCase();
-    if (nameAudio) {
-      setAlbums(
-        listAudio.filter((obj) => obj.filename.toLowerCase().includes(name))
-      );
-    } else {
-      setAlbums(listAudio);
-    }
-  };
-
+  const getItem = (_data, index) => albums[index];
+  const getItemCount = (_data) => 200;
   //console.log(isSearch);
   if (reproductor) {
     return (
@@ -189,7 +75,33 @@ export default function Main() {
             >
               {albums ? (
                 <>
-                  <FlatList
+                  <VirtualizedList
+                    initialNumToRender={15}
+                    renderItem={({ item }) => (
+                      <Musics
+                        album={item}
+                        handleFile={handleFile}
+                        createAudio={createAudio}
+                        handlePosition={handlePosition}
+                      />
+                    )}
+                    keyExtractor={(item) => item.id}
+                    getItemCount={getItemCount}
+                    getItem={getItem}
+                  />
+                </>
+              ) : (
+                <ActivityIndicator />
+              )}
+            </View>
+          </View>
+        </ImageBackground>
+      </>
+    );
+  }
+}
+/*
+<FlatList
                     data={albums}
                     keyExtractor={(album) => album.id}
                     //onEndReached={handleLoadMore}
@@ -202,31 +114,18 @@ export default function Main() {
                         handlePosition={handlePosition}
                       />
                     )}
-                  />
-                </>
-              ) : (
-                <ActivityIndicator />
-              )}
-            </View>
-          </View>
-        </ImageBackground>
-        {fileName && (
-          <Plane
-            fileAudio={fileAudio && fileAudio}
-            isPlaying={isPlaying}
-            status={status}
-            fileName={fileName && fileName}
-            albumSound={albumSound}
-            fileId={parseInt(fileId)}
-            changeSound={changeSound}
-            handleFile={handleFile}
-            positionAudio={positionAudio}
-          />
-        )}
-      </>
-    );
-  }
-}
+{albums.map((item) => {
+                      return (
+                        
+                        <Musics
+                          key={item.id}
+                          album={item}
+                          handleFile={handleFile}
+                          createAudio={createAudio}
+                          handlePosition={handlePosition}
+                        />
+                      );
+                    })}*/
 const styles = StyleSheet.create({
   imgBack: {
     flex: 1,
--- components/Reproductor.jsx
+++ components/Reproductor.jsx
@@ -15,13 +15,16 @@ import {
   RandomIcon,
   RepeatIcon,
   RightIcon,
-  PauseIcon,
+  PauseIcon
 } from './Icons';
 import { useEffect, useState } from 'react';
 import { Slider } from '@miblanchard/react-native-slider';
-import { getPermission, getSound } from '../lib/files';
 import { duration } from '../lib/duration';
-import { pauseAudio, playAudio } from '../lib/playAudio';
+import {
+  pauseAudio,
+  playAudio,
+  getSound
+} from '../lib/playAudio';
 
 export default function Reproductor({
   fileId,
@@ -72,113 +75,108 @@ export default function Reproductor({
 
   const updatePositionSound = async (position) => {
     const positionSound = await fileAudio.setPositionAsync(position * 1000);
-    handlePosition(positionSound.positionMillis / 1000,sound.duration);
+    handlePosition(positionSound.positionMillis / 1000, sound.duration);
   };
-
-  /*if (sound && duration(positionAudio) == seconds && state) {
-    pauseSound();
-    changeSound(id).then((obj) => {
-      setId(obj.num);
-      setSound(obj.sound);
-      setSeconds(obj.seconds);
-      setState(false);
-      isPlaying(false);
-      setPositionAudio(0);
-    });
-  }*/
 
   return (
     <ImageBackground
       source={require('../assets/fondo.jpeg')}
       style={styles.imgBack}
-    >{
-      sound && <View style={styles.contSound}>
-        <View style={styles.contImg}>
-          <Image source={LogoPro} style={styles.img} />
-        </View>
-        <View style={styles.contInterfaz}>
-          <View style={styles.contText}>
-            <Text style={styles.textTitle} className="h-11">
-              {sound.filename}{' '}
-            </Text>
+    >
+      {sound && (
+        <View style={styles.contSound}>
+          <View style={styles.contImg}>
+            <Image source={LogoPro} style={styles.img} />
           </View>
-          <View style={styles.slider}>
-            <Slider
-              minimumTrackTintColor="#18f"
-              maximumTrackTintColor="#fff"
-              minimumValue={0}
-              maximumValue={sound.duration}
-              value={positionAudio}
-              onValueChange={(value) => updatePositionSound(value[0])}
-            />
-            <View
-              style={{ flexDirection: 'row', justifyContent: 'space-between' }}
-            >
-              <Text style={{ color: '#fff' }}>{duration(positionAudio)} </Text>
-              <Text style={styles.duration}>{seconds && seconds} </Text>
+          <View style={styles.contInterfaz}>
+            <View style={styles.contText}>
+              <Text style={styles.textTitle} className="h-11">
+                {sound.filename}{' '}
+              </Text>
+            </View>
+            <View style={styles.slider}>
+              <Slider
+                minimumTrackTintColor="#18f"
+                maximumTrackTintColor="#fff"
+                minimumValue={0}
+                maximumValue={sound.duration}
+                value={positionAudio}
+                onValueChange={(value) => updatePositionSound(value[0])}
+              />
+              <View
+                style={{
+                  flexDirection: 'row',
+                  justifyContent: 'space-between'
+                }}
+              >
+                <Text style={{ color: '#fff' }}>
+                  {duration(positionAudio)}{' '}
+                </Text>
+                <Text style={styles.duration}>{seconds && seconds} </Text>
+              </View>
+            </View>
+
+            <View style={styles.contPlay}>
+              <TouchableHighlight
+                activeOpacity={0.6}
+                underlayColor="#222"
+                style={
+                  randomMode && {
+                    backgroundColor: '#222',
+                    padding: 5,
+                    borderRadius: 50
+                  }
+                }
+                onPress={() => {
+                  randomMode ? randomList(false) : randomList(true);
+                }}
+              >
+                <RandomIcon />
+              </TouchableHighlight>
+              <TouchableHighlight
+                activeOpacity={0.9}
+                underlayColor="#222"
+                style={styles.icons}
+                onPress={() => {
+                  pauseSound();
+                  backSound(fileId).then((obj) => {
+                    setSound(obj.sound);
+                    setSeconds(obj.seconds);
+                    setState(false);
+                    isPlaying(false);
+                  });
+                }}
+              >
+                <LeftIcon />
+              </TouchableHighlight>
+              <Pressable
+                onPress={() => {
+                  status ? playSound() : pauseSound();
+                }}
+              >
+                {!status ? <PauseIcon /> : <PlayIcon />}
+              </Pressable>
+              <TouchableHighlight
+                activeOpacity={0.7}
+                underlayColor="#222"
+                style={styles.icons}
+                onPress={() => {
+                  pauseSound();
+                  changeSound(fileId).then((obj) => {
+                    setSound(obj.sound);
+                    setSeconds(obj.seconds);
+                    setState(false);
+                    isPlaying(false);
+                  });
+                }}
+              >
+                <RightIcon />
+              </TouchableHighlight>
+              <RepeatIcon />
             </View>
           </View>
-
-          <View style={styles.contPlay}>
-            <TouchableHighlight
-              activeOpacity={0.6}
-              underlayColor="#222"
-              style={
-                randomMode && {
-                  backgroundColor: '#222',
-                  padding: 5,
-                  borderRadius: 50
-                }
-              }
-              onPress={() => {
-                randomMode ? randomList(false) : randomList(true);
-              }}
-            >
-              <RandomIcon />
-            </TouchableHighlight>
-            <TouchableHighlight
-              activeOpacity={0.9}
-              underlayColor="#222"
-              style={styles.icons}
-              onPress={() => {
-                pauseSound();
-                backSound(fileId).then((obj) => {
-                  setSound(obj.sound);
-                  setSeconds(obj.seconds);
-                  setState(false);
-                  isPlaying(false);
-                });
-              }}
-            >
-              <LeftIcon />
-            </TouchableHighlight>
-            <Pressable
-              onPress={() => {
-                status ? playSound() : pauseSound();
-              }}
-            >
-              {!status ? <PauseIcon /> : <PlayIcon />}
-            </Pressable>
-            <TouchableHighlight
-              activeOpacity={0.7}
-              underlayColor="#222"
-              style={styles.icons}
-              onPress={() => {
-                pauseSound();
-                changeSound(fileId).then((obj) => {
-                  setSound(obj.sound);
-                  setSeconds(obj.seconds);
-                  setState(false);
-                  isPlaying(false);
-                });
-              }}
-            >
-              <RightIcon />
-            </TouchableHighlight>
-            <RepeatIcon />
-          </View>
         </View>
-      </View>}
+      )}
     </ImageBackground>
   );
 }
